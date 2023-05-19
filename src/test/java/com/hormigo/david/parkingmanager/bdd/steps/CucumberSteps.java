7ec2503ec4762efe32a3d1e6124d90a3e6ec76a5
AlejandroMorenoGarrido <amorgar539@g.educaand.es>
2023-05-19 19:25:54 +0200

    a

--- src/test/java/com/hormigo/david/parkingmanager/bdd/steps/CucumberSteps.java
+++ src/test/java/com/hormigo/david/parkingmanager/bdd/steps/CucumberSteps.java
@@ -3,6 +3,7 @@ package com.hormigo.david.parkingmanager.bdd.steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -12,12 +13,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.hormigo.david.parkingmanager.bdd.CucumberConfiguration;
+import com.hormigo.david.parkingmanager.user.domain.User;
 import com.hormigo.david.parkingmanager.user.service.UserService;
 
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.es.Entonces;
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
@@ -131,6 +134,77 @@ public class CucumberSteps extends CucumberConfiguration {
 
 //---------------------------------------------------------------------------------------------------------
 
+@Dado("un usuario esta en la pagina {}")
+public void openPage(String pageName) {
+    driver.get(getUrlFromPageName(pageName));
+}
+@Dado("el correo {} no esta asignado a otro usuario")
+public void mockUserNotExists(String email){
+    when(mockedUserRepository.findByEmail(email)).thenReturn(null);    
+}
+@Cuando("relleno el campo {} con {}")
+public void populateField(String fieldName,String fieldValue){
+    WebElement inputField = driver.findElement(By.id(getFieldIdFromName(fieldName)));
+    inputField.sendKeys(fieldValue);
+}
+@Cuando("el usuario hace click sobre el botón de {}")
+public void clickButton(String buttonName) {
+    String buttonId = "";
+    switch (buttonName) {
+        case "Usuarios":
+            buttonId = "to-users-link";
+            break;
+        case "Sorteos":
+            buttonId = "to-draws-link";
+            break;
+        case "crear usuario":
+            buttonId = "user-create-button-submit";
+            break;
+        case "crear sorteo":
+            buttonId = "button-create-draw";
+        default:
+            break;
+    }
+    driver.findElement(By.id(buttonId)).click();
+}
+@Entonces("esta en la pagina de {}")
+public void isInPage(String pageName) {
+    assertTrue(driver.getCurrentUrl().equals(getUrlFromPageName(pageName)));
+}
+@Entonces("se ha persistido el usuario en la base de datos")
+public void checkUserWasSaved(){
+    verify(mockedUserRepository,times(1)).save(any(User.class));
+}
+@Entonces("se muestra un campo de {}")
+public void fieldIsDisplayed(String fieldName){
+    String fieldId = getFieldIdFromName(fieldName);
+    WebElement field = driver.findElement(By.id(fieldId));
+    
+    assertTrue(field.isDisplayed());
+}
+private String getUrlFromPageName(String pageName) {
+    String endPoint = "";
+    switch (pageName) {
+        case "inicial":
+            endPoint = "/";
+            break;
+        case "lista de usuarios":
+            endPoint = "/users";
+            break;
+        case "lista de sorteos":
+            endPoint = "/draws";
+            break;
+        case "creación de usuarios":
+            endPoint = "/newUser";
+            break;
+        case "creacion de sorteos":
+            endPoint = "/newDraw";
+            break;
+        default:
+            break;
+    }
+    return getUrlFromEndPoint(endPoint);
+}
 
 
 
